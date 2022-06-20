9a6220bfa3796d2955351db6352abd999751c72b
Arzeezar <72872978+Arzeezar@users.noreply.github.com>
2022-06-21 01:04:05 +0700

    add registration function to firestore

--- lib/login/regis.dart
+++ lib/login/regis.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:form_field_validator/form_field_validator.dart';
 import '../calculator/cal_page.dart';
@@ -14,89 +16,110 @@ class _regisPage extends State<regis> {
 
   final formKey = GlobalKey<FormState>();
   Profile profile = Profile();
+  // เตรียม firebase
+  // final Future<FirebaseApp> firebase = Firebase.initializeApp();
+  CollectionReference _memberCollections = FirebaseFirestore.instance.collection("member");
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('สมัครสมาชิก'),
-      ),
-      body: Stack(
-            children: [
-            SingleChildScrollView(
-              child: Container(
-                padding: EdgeInsets.only(
-                  top: 35,
-                  left: 35,
-                  right: 35,
-                ),
-                child: Form(
-                  key: formKey,
-                  child: Column(
-                  children: <Widget>[
-                    SizedBox(height: 30.0),
-                    TextFormField(
-                      // controller: roomCost,
-                      validator:
-                          RequiredValidator(errorText: "กรุณากรอกชื่อผู้ใช้"),
-                      onSaved: (String? username) => profile.username = username,
-                      keyboardType: TextInputType.text,
-                      decoration: InputDecoration(
-                        labelText: 'username',
-                        labelStyle: TextStyle(fontSize: 12),
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(15),
+    return FutureBuilder(
+      future: Firebase.initializeApp(),
+      builder: (context,snapshot){
+        if(snapshot.hasError){
+          return Scaffold(
+            appBar: AppBar(title: Text("error"),),
+            body: Center(child: Text("${snapshot.error}"),),
+          );
+        }
+        if(snapshot.connectionState == ConnectionState.done){
+          return Scaffold(
+              appBar: AppBar(
+                title: const Text('สมัครสมาชิก'),
+              ),
+              body: Stack(
+                    children: [
+                    SingleChildScrollView(
+                      child: Container(
+                        padding: EdgeInsets.only(
+                          top: 35,
+                          left: 35,
+                          right: 35,
+                        ),
+                        child: Form(
+                          key: formKey,
+                          child: Column(
+                          children: <Widget>[
+                            SizedBox(height: 30.0),
+                            TextFormField(
+                              // controller: roomCost,
+                              validator: RequiredValidator(errorText: "กรุณากรอกชื่อผู้ใช้"),
+                              onSaved: (String? username) => profile.username = username,
+                              keyboardType: TextInputType.text,
+                              decoration: InputDecoration(
+                                labelText: 'username',
+                                labelStyle: TextStyle(fontSize: 12),
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(15),
+                                ),
+                              ),
+                            ),
+                            SizedBox(height: 30.0),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: <Widget>[
+                                Flexible(
+                                  child: TextFormField(
+                                    // controller: oldUnit,
+                                    validator: RequiredValidator(
+                                        errorText: "กรุณากรอกรหัสผ่าน"),
+                                    onSaved: (String? password) =>
+                                        profile.password = password,
+                                    obscureText: true,
+                                    decoration: InputDecoration(
+                                      labelText: 'password',
+                                      labelStyle: TextStyle(fontSize: 12),
+                                      border: OutlineInputBorder(
+                                        borderRadius: BorderRadius.circular(15),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                                SizedBox(width: 10),
+                              ],
+                            ),
+                            SizedBox(height: 30.0),
+                            ElevatedButton(
+                              child: const Text('สมัคร'),
+                              onPressed: () async{
+                                if (formKey.currentState!.validate()) {
+                                  formKey.currentState!.save();
+                                  await _memberCollections.add({
+                                    "username": profile.username,
+                                    "password": profile.password,
+                                    "role": "admin"
+                                  });
+                                  formKey.currentState!.reset();
+                                }
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => FirstScreen()),
+                                );
+                              },
+                            ),
+                          ],
+                        ),
                         ),
                       ),
                     ),
-                    SizedBox(height: 30.0),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget>[
-                        Flexible(
-                          child: TextFormField(
-                            // controller: oldUnit,
-                            validator: RequiredValidator(
-                                errorText: "กรุณากรอกรหัสผ่าน"),
-                            onSaved: (String? password) =>
-                                profile.password = password,
-                            obscureText: true,
-                            decoration: InputDecoration(
-                              labelText: 'password',
-                              labelStyle: TextStyle(fontSize: 12),
-                              border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(15),
-                              ),
-                            ),
-                          ),
-                        ),
-                        SizedBox(width: 10),
-                      ],
-                    ),
-                    SizedBox(height: 30.0),
-                    ElevatedButton(
-                      child: const Text('สมัคร'),
-                      onPressed: () {
-                        if (formKey.currentState!.validate()) {
-                          formKey.currentState!.save();
-                          print(
-                              "user = ${profile.username}, pass = ${profile.password}");
-                          formKey.currentState!.reset();
-                        }
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => FirstScreen()),
-                        );
-                      },
-                    ),
                   ],
                 ),
-                ),
-              ),
-            ),
-          ],
-        ),
-      );
+              );
+        }
+        return Scaffold(
+          body: Center(child: CircularProgressIndicator(),),
+        );
+      }
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'calculator/cal_page.dart';
 import 'report/report_page.dart';
 import 'login/login_page.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MaterialApp(
     title: 'Rent app',
     home: login(),
